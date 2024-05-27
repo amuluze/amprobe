c0f59e3111d587e47a3f186880eaf8b7bf4bb84c
Amu <wangjialong89@yeah.net>
2024-05-27 22:45:53 +0800

    feat: 二级路由设置

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-import Icons from 'unplugin-icons/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
 import IconsResolver from 'unplugin-icons/resolver'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import Icons from 'unplugin-icons/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/', // 设置打包路径
+    base: './', // 设置打包路径
     plugins: [
         vue(),
         AutoImport({
