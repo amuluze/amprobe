413473182fb1993a419410d736d1332e5aed7e25
jialong.wang <jialong.wang@chaitin.com>
2024-06-13 17:15:40 +0800

    feat: update Dockerfile

--- run.sh
+++ run.sh
@@ -3,13 +3,14 @@
 # Author     : Amuluze
 # Date       : 2024/05/24 10:47:05
 # Description: 一键部署脚本
-#              sudo make -p /data
-#              sudo unzip amprobe.installer.zip -d /data
-#              sudo cd /data/amvector
-#              sudo ./amvector install --conf=/data/amvector/configs/config.toml
-#              sudo ./amvector start
-#              sudo docker load -i amprobe.tar.gz/amprobe.arm.tar.gz
-#              sudo docker run -itd --name amprobe -p 80:80 \
+#              make -p /data
+#              unzip amprobe.installer.zip -d /data
+#              cd /data/amvector
+#              ./amvector install --conf=/data/amvector/configs/config.toml
+#              ./amvector start
+#               cd /data
+#              docker load -i amprobe.tar.gz
+#              docker run -itd --name amprobe -p 80:80 \
 #               -v /data/amprobe/configs:/app/configs \
 #               -v /data/amprobe/nginx/nginx.conf:/etc/nginx/nginx.conf \
 #               -v /data/amvector/amvector.socket:/app/amvector.socket \
