5067698b4ff778e72ecee8cb8a438d76c97a1a6d
Amu <wangjialong89@yeah.net>
2024-08-11 15:33:54 +0800

    feat: v1.3.6

--- web/dist/js/404.B9JLcBSx.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.DuNUv9gK.js";const e=r({},[["render",function(r,e){return"404"}]]);export{e as default};
---
+++ web/dist/js/index.Dtf5szNq.js
@@ -0,0 +1,1 @@
+import{r as a}from"./index.Bs0cFoC_.js";const o="/app/img/amprobe.Da9pVUU8.png";function t(o){return a.post("/api/v1/auth/login",o)}function p(){return a.post("/api/v1/auth/logout",{})}export{o as _,p as a,t as l};
